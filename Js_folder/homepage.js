be8ba2a314451cf7e1178564e7a09207cbad6ed6
umangsaluja <umangsaluja90@gmail.com>
2020-11-29 12:15:04 +0530

    Final Changes made

--- Js_folder/homepage.js
+++ Js_folder/homepage.js
@@ -1,49 +1,70 @@
 // for scrolling effect 
-window.addEventListener("scroll", function(){
-    		var header = document.getElementById('header2');
-    		header.classList.toggle("sticky", window.scrollY > 0);
-    		header.classList.toggle("newClass",window.scrollY > 0);
-    	})
+window.addEventListener("scroll", function() {
+    var header = document.getElementById('header2');
+    header.classList.toggle("sticky", window.scrollY > 0);
+    header.classList.toggle("newClass", window.scrollY > 0);
+})
 //for toggle  menu bars
-$(document).ready(function(){
-   $('.bars').click(function(){
-   	$('.menu').slideToggle();
-   });
-//for gallery data filleds
-$( '.js-filter' ).on( 'click', function() {
-  
-  var $attribute = $(this).attr('data-filter') ;$('li').removeClass("active");
-$(this). addClass("active");
-  console.log($attribute);
-  if ( $attribute == 'All' ) {
-    $( '.js-filterable' ).removeClass( 'is-hidden' );    
-  } else {
-    $( '.js-filterable' ).addClass( 'is-hidden' );
-    $( '.js-filterable[data-filter=' + $attribute + ']' ).removeClass( 'is-hidden' );
-  }
-  
+$(document).ready(function() {
+    $('.bars').click(function() {
+        $('.menu').slideToggle();
+    });
+    //for gallery data filleds
+    $('.js-filter').on('click', function() {
+        var $attribute = $(this).attr('data-filter');
+        $('li').removeClass("active");
+        $(this).addClass("active");
+        console.log($attribute);
+        if ($attribute == 'All') {
+            $('.js-filterable').removeClass('is-hidden');
+        } else {
+            $('.js-filterable').addClass('is-hidden');
+            $('.js-filterable[data-filter=' + $attribute + ']').removeClass('is-hidden');
+        }
+    });
+    //for about up arrow
+    $('.next').click(function() {
+        $('.slide').find('div.active').next().addClass('active');
+        $('.slide').find('div.active').prev().removeClass('active');
+    })
+    //for about down arrow
+    $('.prev').click(function() {
+        $('.slide').find('div.active').prev().addClass('active');
+        $('.slide').find('div.active').next().removeClass('active');
+    })
+
+    /*===== SCROLL REVEAL ANIMATION =====*/
+    const sr = ScrollReveal({
+        origin: 'top',
+        distance: '80px',
+        duration: 2000,
+        reset: true
+    })
+
+    /*SCROLL ABOUT*/
+    sr.reveal('.aboutLeftContent', { delay: 300 })
+    sr.reveal('#aboutHeading', { delay: 350 })
+    sr.reveal('#aboutPara', { delay: 400 })
+    sr.reveal('.galleryUl', { delay: 450 })
+    /*
+    sr.reveal('.Bio-group', { delay: 500 })*/
+    sr.reveal('.product', { delay: 550 })
+    sr.reveal('.linkedIn-li', { delay: 600, interval: 200 })
+
+    /*SCROLL SKILLS*/
+    sr.reveal('.MainHeading', {})
+    sr.reveal('.left-col h3', { delay: 40 })
+    sr.reveal('.left-col p', { delay: 100 })
+    sr.reveal('.wrapper', { delay: 160 })
+    sr.reveal('.mid-col h3', { delay: 40 })
+    sr.reveal('.mid-col p', { delay: 100 })
+    sr.reveal('.right-col h3', { delay: 40 })
+    sr.reveal('.right-col ul li', { delay: 100 })
+
 });
+// banner caro
 
-//for animation 
-  $(window).scroll(function(){
-      var positionTop =$(document).scrollTop();
-      console.log(positionTop);
-      if((positionTop > 450) && (positionTop < 900)){  $("#aboutHeading").addClass("animated rotateInUpRight");
-        $("#aboutPara").addClass("animated zoomInUp");}
-        if((positionTop > 916) && (positionTop < 2355)){   $("#galleryHeading").addClass("animated bounceInLeft");
-      }
-   });
-//for about up arrow
-   $('.next').click(function(){
-      $('.slide').find('div.active').next().addClass('active');  $('.slide').find('div.active').prev().removeClass('active');
-   })
-  //for about down arrow
-   $('.prev').click(function(){
-      $('.slide').find('div.active').prev().addClass('active');  $('.slide').find('div.active').next().removeClass('active');
-   })
-
-});// carousal
-
+// carousal
 var mySwiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
@@ -56,11 +77,12 @@ var mySwiper = new Swiper('.swiper-container', {
             slidesPerView: 1,
             spaceBetween: 0,
         },
+
         767: {
             slidesPerView: 1,
-            
+
 
         },
-        1000: { slidesPerView: 4,  }
+        1000: { slidesPerView: 4, }
     }
 })
